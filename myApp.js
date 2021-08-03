3ac90cd269cd332f0c4ed738917d3f277fb7276b
Timofey Trofimov <tumoxep@gmail.com>
2021-08-04 01:11:39 +0300

    Serve Static Assets 2

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 console.log('Hello World');
-app.use(express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
