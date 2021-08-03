29beb08ec303cdeb27714a8421b43d674dd9c34a
Timofey Trofimov <tumoxep@gmail.com>
2021-08-04 01:15:50 +0300

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -5,9 +5,9 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-
-
+app.get('/json', function(req, res) {
+  res.json({ message: 'Hello json' });
+});
 
 
 
