3c5e228d1b80f953fdaf0602887bd147c6324c9d
Тимофей Трофимов <t.trofimov@just-work.org>
2021-08-05 23:11:04 +0300

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -2,16 +2,16 @@ var express = require('express');
 var app = express();
 console.log('Hello World');
 app.use('/public', express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 app.get('/json', function(req, res) {
   res.json({ message: process.env.MESSAGE_STYLE === 'uppercase' ? 'HELLO JSON' : 'Hello json' });
 });
-
-
-
-
 
 
 
